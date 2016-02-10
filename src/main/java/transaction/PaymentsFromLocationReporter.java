19df0d5be6ad0447f266c991710b620b2f5b0987
Colin Lam <colinlam@squareup.com>
2016-02-09 18:04:40 -0800

    additional flow logic

--- src/main/java/transaction/PaymentsFromLocationReporter.java
+++ src/main/java/transaction/PaymentsFromLocationReporter.java
@@ -1,7 +1,10 @@
 package transaction;
 
+import java.util.HashMap;
+
 import org.mule.api.MuleEventContext;
 import org.mule.api.lifecycle.Callable;
+import org.mule.api.transport.PropertyScope;
 
 import com.squareup.connect.Payment;
 import com.squareup.connect.SquareClient;
@@ -12,14 +15,30 @@ public class PaymentsFromLocationReporter implements Callable {
 	public Object onCall(MuleEventContext eventContext) throws Exception {
 		SquareAccount sa = (SquareAccount) eventContext.getMessage().getPayload();
 		
+		// TODO(colinlam): make this configurable
 		SquareClient client = new SquareClient(sa.getAccessToken(), "https://connect.squareupstaging.com/v1", sa.getSquareId());
+		
+		String beginTime = eventContext.getMessage().getProperty("begin_time", PropertyScope.SESSION, "");
+		String endTime = eventContext.getMessage().getProperty("end_time", PropertyScope.SESSION, "");
+		String order = eventContext.getMessage().getProperty("order", PropertyScope.SESSION, "");
+		String limit = eventContext.getMessage().getProperty("limit", PropertyScope.SESSION, "");
+		
+		HashMap<String,String> map = new HashMap<String,String>();
+		if (!beginTime.equals("")) {
+			map.put("begin_time", beginTime);			
+		}
+		if (!endTime.equals("")) {
+			map.put("end_time", endTime);			
+		}
+		if (!order.equals("")) {
+			map.put("order", order);			
+		}
+		if (!limit.equals("")) {
+			map.put("limit", limit);			
+		}
+		
+        Payment[] payments = client.payments().list(map);
         
-        Payment[] payments = client.payments().get();
-        
-        if (payments != null) {
-        	return payments;
-        } else {
-        	return null;
-        }
+        return payments;
 	}
 }
