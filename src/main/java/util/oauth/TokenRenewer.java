f020b0074418473f84d923eb4200a5597106600c
Ben Hartard <bhartard@squareup.com>
2016-06-08 16:46:10 -0700

    Remove colin on emails, fix access token cron to UPDATE, fix token cron to use Quartz over polling

--- src/main/java/util/oauth/TokenRenewer.java
+++ src/main/java/util/oauth/TokenRenewer.java
@@ -41,12 +41,11 @@ public class TokenRenewer implements Callable {
 	public Object onCall(MuleEventContext eventContext) throws Exception {
 		@SuppressWarnings("unchecked")
 		Map<String,Object> m = (Map<String, Object>) eventContext.getMessage().getPayload();
-		
+
 		Integer id = (Integer) m.get("id");
 		String token = (String) m.get("token");
 		String connectApp = (String) m.get("connectApp");
-		String deployment = (String) m.get("deployment");
-		
+
 		String secret = "";
 		if (connectApp != null && connectApp.equals(multiunitId)) {
 			secret = multiunitSecret;
@@ -55,14 +54,12 @@ public class TokenRenewer implements Callable {
 		} else {
 			throw new Exception("connect app '" + connectApp + "' associated with this token is not an official Managed Integrations application");
 		}
-		
+
 		SquareClient client = new SquareClient(token, apiUrl);
 		OAuthToken newToken = client.oauth().renewToken(connectApp, secret);
-		
-		eventContext.getMessage().setProperty("oldId", id, PropertyScope.INVOCATION);
-		eventContext.getMessage().setProperty("deployment", deployment, PropertyScope.INVOCATION);
-		eventContext.getMessage().setProperty("connectApp", connectApp, PropertyScope.INVOCATION);
-		
+
+		eventContext.getMessage().setProperty("tokenId", id, PropertyScope.INVOCATION);
+
 		return newToken;
 	}
 }
