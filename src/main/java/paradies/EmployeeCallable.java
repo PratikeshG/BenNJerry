4a19287cee185c4a61af1bda131cbe9955269c01
Ben Hartard <bhartard@squareup.com>
2016-08-28 23:34:30 -0700

    WIP

--- src/main/java/paradies/EmployeeCallable.java
+++ src/main/java/paradies/EmployeeCallable.java
@@ -1,15 +1,9 @@
 package paradies;
 
-import paradies.Util;
-import paradies.pandora.CatalogEntry;
 import util.SquarePayload;
 
 import org.mule.api.MuleMessage;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -19,9 +13,9 @@ import org.mule.api.lifecycle.Callable;
 import org.mule.api.transport.PropertyScope;
 
 import com.squareup.connect.SquareClient;
+import com.squareup.connect.diff.EmployeeChangeRequest;
 import com.squareup.connect.Employee;
 import com.squareup.connect.EmployeeRole;
-import com.squareup.connect.diff.EmployeeChangeRequest;
 
 public class EmployeeCallable implements Callable {
 
@@ -34,69 +28,27 @@ public class EmployeeCallable implements Callable {
 		String apiVersion = message.getProperty("apiVersion", PropertyScope.SESSION);
 		SquareClient client = new SquareClient(sqPayload.getAccessToken(), apiUrl, apiVersion, sqPayload.getMerchantId(), sqPayload.getLocationId());
 		
-		// Find the owner role
-		ArrayList<EmployeeRole> ownerRoles = new ArrayList<EmployeeRole>();
-		String managerRole = "";
-		String cashierRole = "";
-		EmployeeRole[] currentRoles = client.roles().list();
-		for (EmployeeRole role : currentRoles) {
-			if (role.isOwner()) {
-				ownerRoles.add(role);
-			} else if (role.getName().equals("Cashier")) {
-				cashierRole = role.getId();
-			} else if (role.getName().equals("Store Manager")) {
-				managerRole = role.getId();
-			}
-		}
-		ArrayList<Employee> currentEmployees = new ArrayList<Employee>(Arrays.asList(client.employees().list()));
-		ArrayList<Employee> updatedEmployees = new ArrayList<Employee>();
-
-		// Process file and create matching employee objects
-		
 		byte[] ftpPayload = eventContext.getMessage().getProperty("ftpPayload", PropertyScope.SESSION);
-		InputStream stream = new ByteArrayInputStream(ftpPayload);
-        BufferedReader bfReader = new BufferedReader(new InputStreamReader(stream));
+		EmployeeRole[] currentRoles = client.roles().list();
+		
+		EmployeeGenerator employeeGenerator = new EmployeeGenerator();
+		ArrayList<Employee> updatedEmployees = (ArrayList<Employee>) employeeGenerator.parsePayload(ftpPayload, currentRoles);
 
-        String line = "";
-        while((line = bfReader.readLine()) != null) {
-        	if (line.length() < 20) {
-        		continue;
-        	}
-        	
-        	String[] fields = line.split(",");
-
-        	// Delete employee command
-        	if (fields[0].equals("3")) {
-        		continue;
-        	}
-
-        	String role = fields[20].equals("5") ? managerRole : cashierRole;
-
-        	Employee newEmployee = new Employee();
-        	newEmployee.setExternalId(fields[2]);
-        	newEmployee.setFirstName(fields[4]);
-        	newEmployee.setLastName(fields[23]);
-        	newEmployee.setRoleIds(new String[]{role});
-
-        	updatedEmployees.add(newEmployee);
-        }
-
-        if(stream != null) {
-        	stream.close();
-        }
-
+		// Perform diff
 		HashSet<Object> ignoreEmployeeFields = new HashSet<Object>();
 		ignoreEmployeeFields.add(Employee.Field.ID);
 		ignoreEmployeeFields.add(Employee.Field.STATUS);
 		ignoreEmployeeFields.add(Employee.Field.CREATED_AT);
 		ignoreEmployeeFields.add(Employee.Field.UPDATED_AT);
 		ignoreEmployeeFields.add(Employee.Field.EMAIL);
-
+		
+		ArrayList<Employee> currentEmployees = new ArrayList<Employee>(Arrays.asList(client.employees().list()));
+		ArrayList<EmployeeRole> ownerRoles = (ArrayList<EmployeeRole>) EmployeeGenerator.getOwnerRoles(currentRoles);
+		
 		EmployeeChangeRequest ecr = EmployeeChangeRequest.diff(currentEmployees, updatedEmployees, ownerRoles, ignoreEmployeeFields);
-
 		ecr.setSquareClient(client);
 		ecr.call();
-
+		
 		return updatedEmployees;
 	}
 }
