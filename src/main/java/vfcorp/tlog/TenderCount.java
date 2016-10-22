749ac27d745ec39cf13bba23ad013462428d1218
Ben Hartard <bhartard@squareup.com>
2016-10-21 17:50:45 -0700

    TNF - Ignore cash and no_sale transactions

--- src/main/java/vfcorp/tlog/TenderCount.java
+++ src/main/java/vfcorp/tlog/TenderCount.java
@@ -4,90 +4,96 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.squareup.connect.Payment;
+
 import vfcorp.FieldDetails;
 import vfcorp.Record;
 
-import com.squareup.connect.Payment;
-
 public class TenderCount extends Record {
 
-	private static Map<String,FieldDetails> fields;
-	private static int length;
-	private static String id;
-	
-	static {
-		fields = new HashMap<String,FieldDetails>();
-		length = 41;
-		id = "034";
-		
-		fields.put("Identifier", new FieldDetails(3, 1, ""));
-		fields.put("Tender Code", new FieldDetails(8, 4, "From table, left justified"));
-		fields.put("Number In Drawer", new FieldDetails(6, 12, "zero filled"));
-		fields.put("Amount In Drawer", new FieldDetails(10, 18, "zero filled"));
-		fields.put("Amount In Drawer Sign", new FieldDetails(1, 28, "1 = Negative, zero filled"));
-		fields.put("Amount Counted", new FieldDetails(10, 29, "zero filled"));
-		fields.put("Amount Counted Sign", new FieldDetails(1, 39, "1 = Negative, zero filled"));
-		fields.put("Currency Indicator", new FieldDetails(1, 40, "1 = Alternate, 0 = Primary, zero filled"));
-		fields.put("Counted Indicator", new FieldDetails(1, 41, "1 = Dollar, 0 = Quantity, zero filled"));
-	}
-	
-	public TenderCount() {
-		super();
-	}
+    private static Map<String, FieldDetails> fields;
+    private static int length;
+    private static String id;
 
-	public TenderCount(String record) {
-		super(record);
-	}
+    static {
+        fields = new HashMap<String, FieldDetails>();
+        length = 41;
+        id = "034";
 
-	@Override
-	public Map<String, FieldDetails> getFields() {
-		return fields;
-	}
+        fields.put("Identifier", new FieldDetails(3, 1, ""));
+        fields.put("Tender Code", new FieldDetails(8, 4, "From table, left justified"));
+        fields.put("Number In Drawer", new FieldDetails(6, 12, "zero filled"));
+        fields.put("Amount In Drawer", new FieldDetails(10, 18, "zero filled"));
+        fields.put("Amount In Drawer Sign", new FieldDetails(1, 28, "1 = Negative, zero filled"));
+        fields.put("Amount Counted", new FieldDetails(10, 29, "zero filled"));
+        fields.put("Amount Counted Sign", new FieldDetails(1, 39, "1 = Negative, zero filled"));
+        fields.put("Currency Indicator", new FieldDetails(1, 40, "1 = Alternate, 0 = Primary, zero filled"));
+        fields.put("Counted Indicator", new FieldDetails(1, 41, "1 = Dollar, 0 = Quantity, zero filled"));
+    }
 
-	@Override
-	public int getLength() {
-		return length;
-	}
+    public TenderCount() {
+        super();
+    }
 
-	@Override
-	public String getId() {
-		return id;
-	}
-	
-	public TenderCount parse(String tenderCode, List<Payment> squarePaymentsList) throws Exception {
-		int number = 0;
-		int amount = 0;
-		
-		for (Payment squarePayment : squarePaymentsList) {
-			for (com.squareup.connect.Tender tender : squarePayment.getTender()) {
-				if ((Tender.TENDER_CODE_CASH.equals(tenderCode) && tender.getType().equals("CASH")) ||
-						(Tender.TENDER_CODE_AMEX.equals(tenderCode) && tender.getType().equals("CREDIT_CARD") && tender.getCardBrand().equals("AMERICAN_EXPRESS")) ||
-						(Tender.TENDER_CODE_DISCOVER.equals(tenderCode) && tender.getType().equals("CREDIT_CARD") && tender.getCardBrand().equals("DISCOVER")) ||
-						(Tender.TENDER_CODE_VISA.equals(tenderCode) && tender.getType().equals("CREDIT_CARD") && tender.getCardBrand().equals("VISA")) ||
-						(Tender.TENDER_CODE_MASTERCARD.equals(tenderCode) && tender.getType().equals("CREDIT_CARD") && tender.getCardBrand().equals("MASTER_CARD")) ||
-						(Tender.TENDER_CODE_JCB.equals(tenderCode) && tender.getType().equals("CREDIT_CARD") && tender.getCardBrand().equals("JCB")) ||
-						(Tender.TENDER_CODE_GIFT_CERTIFICATE.equals(tenderCode) && tender.getType().equals("OTHER") && "MERCHANT_GIFT_CARD".equals(tender.getName())) ||
-						(Tender.TENDER_CODE_98.equals(tenderCode) && tender.getType().equals("UNKNOWN"))) {
-					
-					number += 1;
-					if (tender.getTenderedMoney() != null) {
-						amount += tender.getTenderedMoney().getAmount();
-					} else {
-						amount += tender.getTotalMoney().getAmount();
-					}
-				}
-			}
-		}
-		
-		putValue("Tender Code", tenderCode);
-		putValue("Number In Drawer", "" + number);
-		putValue("Amount In Drawer", "" + amount);
-		putValue("Amount In Drawer Sign", amount >= 0 ? "0" : "1");
-		putValue("Amount Counted", "" + amount);
-		putValue("Amount Counted Sign", amount >= 0 ? "0" : "1");
-		putValue("Currency Indicator", "0"); // not supported
-		putValue("Counted Indicator", "1"); // 1 is "dollars"; other value not supported
-		
-		return this;
-	}
+    public TenderCount(String record) {
+        super(record);
+    }
+
+    @Override
+    public Map<String, FieldDetails> getFields() {
+        return fields;
+    }
+
+    @Override
+    public int getLength() {
+        return length;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    public TenderCount parse(String tenderCode, List<Payment> squarePaymentsList) throws Exception {
+        int number = 0;
+        int amount = 0;
+
+        for (Payment squarePayment : squarePaymentsList) {
+            for (com.squareup.connect.Tender tender : squarePayment.getTender()) {
+                if ((Tender.TENDER_CODE_CASH.equals(tenderCode) && tender.getType().equals("CASH"))
+                        || (Tender.TENDER_CODE_AMEX.equals(tenderCode) && tender.getType().equals("CREDIT_CARD")
+                                && tender.getCardBrand().equals("AMERICAN_EXPRESS"))
+                        || (Tender.TENDER_CODE_DISCOVER.equals(tenderCode) && tender.getType().equals("CREDIT_CARD")
+                                && tender.getCardBrand().equals("DISCOVER"))
+                        || (Tender.TENDER_CODE_VISA.equals(tenderCode) && tender.getType().equals("CREDIT_CARD")
+                                && tender.getCardBrand().equals("VISA"))
+                        || (Tender.TENDER_CODE_MASTERCARD.equals(tenderCode) && tender.getType().equals("CREDIT_CARD")
+                                && tender.getCardBrand().equals("MASTER_CARD"))
+                        || (Tender.TENDER_CODE_JCB.equals(tenderCode) && tender.getType().equals("CREDIT_CARD")
+                                && tender.getCardBrand().equals("JCB"))
+                        || (Tender.TENDER_CODE_GIFT_CERTIFICATE.equals(tenderCode) && tender.getType().equals("OTHER")
+                                && "MERCHANT_GIFT_CARD".equals(tender.getName()))
+                        || (Tender.TENDER_CODE_98.equals(tenderCode) && tender.getType().equals("UNKNOWN"))) {
+
+                    number += 1;
+                    if (tender.getTenderedMoney() != null) {
+                        amount += tender.getTenderedMoney().getAmount();
+                    } else {
+                        amount += tender.getTotalMoney().getAmount();
+                    }
+                }
+            }
+        }
+
+        putValue("Tender Code", tenderCode);
+        putValue("Number In Drawer", "" + number);
+        putValue("Amount In Drawer", "" + amount);
+        putValue("Amount In Drawer Sign", amount >= 0 ? "0" : "1");
+        putValue("Amount Counted", "" + amount);
+        putValue("Amount Counted Sign", amount >= 0 ? "0" : "1");
+        putValue("Currency Indicator", "0"); // not supported
+        putValue("Counted Indicator", "1"); // 1 is "dollars"
+
+        return this;
+    }
 }
