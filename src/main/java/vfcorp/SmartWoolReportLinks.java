3d081ea945be3da65c3fbe7023ab50aa62c78079
wtsang <wtsang@squareup.com>
2016-11-07 10:34:28 -0800

    small fixes for initial commit including properties file changes, comments, URI building, and adding HTTP trigger

--- src/main/java/vfcorp/SmartWoolReportLinks.java
+++ src/main/java/vfcorp/SmartWoolReportLinks.java
@@ -7,10 +7,12 @@ import org.mule.api.transport.PropertyScope;
 
 import com.squareup.connect.v2.SquareClientV2;
 import com.squareup.connect.v2.Location;
-import com.squareup.connect.v2.LocationsAdapter;
 
 import util.SquarePayload;
 import util.TimeManager;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SmartWoolReportLinks implements Callable {
     
@@ -18,22 +20,22 @@ public class SmartWoolReportLinks implements Callable {
     public Object onCall(MuleEventContext eventContext) throws Exception {
         MuleMessage message = eventContext.getMessage();
       
-        // session var for API requests
+        // session var for API requests and urls
         String apiUrl = message.getProperty("apiUrl", PropertyScope.SESSION);
+        String domainUrl = message.getProperty("domainUrl", PropertyScope.SESSION);
+        
         // set SquareClientV2 for API calls
         SquarePayload payload = (SquarePayload) message.getPayload();
-        SquareClientV2 client = new SquareClientV2(apiUrl,payload.getAccessToken());
+        SquareClientV2 client = new SquareClientV2(apiUrl, payload.getAccessToken());
         
         // final payload as emailBody in html
         StringBuilder emailBody = new StringBuilder();
         
         // connect.V2 location objects
-        LocationsAdapter locationAdapter = null;
         Location[] locations = null;
         
         // get list of SmartWool locations using SquareClientV2
-        locationAdapter = client.locations();
-        locations = locationAdapter.list();
+        locations = client.locations().list();
         
         // set email html tags
         emailBody.append("<!DOCTYPE html>");
@@ -41,9 +43,9 @@ public class SmartWoolReportLinks implements Callable {
         emailBody.append("<body>");
         emailBody.append("Please see the links below to download the prior day's transaction, item, and deposit reports.<br/>");
         
-        // sort ReportLinks according to timezone
+        // list transaction, item, and deposit report links on a per location basis
         for (int i = 0; i < locations.length; i++) {
-        	ReportLinks reportsTemp = new ReportLinks(locations[i]);
+        	ReportLinks reportsTemp = new ReportLinks(domainUrl, locations[i]);
         	
         	emailBody.append("<h4>" + reportsTemp.getName()  + "</h4>");
         	emailBody.append("<ul>");
@@ -52,9 +54,9 @@ public class SmartWoolReportLinks implements Callable {
         	emailBody.append("<li><a href='" + reportsTemp.getDepositUrl() + "'>Deposit Report</a></li>");
         	emailBody.append("</ul>");
         	
-        }       
+        }
         emailBody.append("</body></html>");
-                   
+        
         return emailBody.toString();
         
     }
@@ -65,94 +67,69 @@ public class SmartWoolReportLinks implements Callable {
      *	       2) itemDetail: https://squareup.com/v2/reports/items.csv?begin_time=2016-11-02T00:00:00-07:00&end_time=2016-11-03T00:00:00-07:00&Time-Zone=America/Los_Angeles&event_based=true&locale=en&subunit_merchant_token[]=FXQFKY6YHS7FM
      *         3) deposit: https://squareup.com/v2/reports/deposit-details.csv?begin_time=2016-11-02T00:00:00-07:00&end_time=2016-11-03T00:00:00-07:00&Time-Zone=America/Los_Angeles&event_based=true&locale=en&subunit_merchant_token[]=3MH6E2GMAP5A5
      *
-     * Examples for TimeManager.java:
-     *     Today: 11/03, Time: ~2:50pm
-     *
-     *     TimeManager.getPastDayInterval(0, 0, "GMT-00:00").get("begin_time") = 2016-11-03T00:00:00Z 
-     *     TimeManager.getPastDayInterval(0, 0, "GMT-00:00").get("end_time") = 2016-11-03T21:45:00Z
-     *
-     *     TimeManager.getPastDayInterval(0, 0, "GMT-07:00").get("begin_time") = 2016-11-03T00:00:00-07:00
-     *     TimeManager.getPastDayInterval(0, 0, "GMT-07:00").get("end_time") = 2016-11-03T14:53:12-07:00
-     *
-     *     TimeManager.getPastDayInterval(1, 0, "GMT-07:00").get("begin_time") = 2016-11-03T00:00:00-07:00
-     *     TimeManager.getPastDayInterval(1, 0, "GMT-07:00").get("end_time") = 2016-11-03T14:56:28-07:00
-     *
-     *     TimeManager.getPastDayInterval(0, -1, "GMT-07:00").get("begin_time") = 2016-11-04T00:00:00-07:00
-     *     TimeManager.getPastDayInterval(0, -1, "GMT-07:00").get("end_time") = 2016-11-04T14:58:41-07:00 
-     *
-     *     TimeManager.getPastDayInterval(0, 1, "GMT-07:00").get("begin_time") = 2016-11-02T00:00:00-07:00
-     *     TimeManager.getPastDayInterval(0, 1, "GMT-07:00").get("end_time") = 2016-11-02T23:59:59-07:00
-     *
-     *     NOTE: this produces the same result as above with different parameters!!
-     *     TimeManager.getPastDayInterval(1, 1, "GMT-07:00").get("begin_time") = 2016-11-02T00:00:00-07:00
-     *     TimeManager.getPastDayInterval(1, 1, "GMT-07:00").get("end_time") = 2016-11-02T23:59:59-07:00
-     *
-     *	   IN-USE: this is a hack to get the date range for the report links
-     *     TimeManager.getPastDayInterval(0, 1, "GMT-07:00").get("begin_time") = 2016-11-02T00:00:00-07:00
-     *     TimeManager.getPastDayInterval(0, 0, "GMT-07:00").get("begin_time") = 2016-11-03T00:00:00-07:00
+     *     NOTE: Assume this runs at 7:00 am EST
      */
-    
-    // Assume this runs at 8:00 am (cron time, begin/end times of report, day of report)
-    class ReportLinks {    	
-    	private String transactionUrl;
-    	private String itemDetailUrl;
-    	private String depositUrl;
-    	private String beginTime;
-    	private String endTime;
-    	private String timezone;
-    	private String eventBased;
-    	private String locale;
-    	private String locationToken;
+    class ReportLinks {
+		private final String transactionPath = "/v2/reports/transactions.csv?";
+		private final String itemDetailPath = "/v2/reports/items.csv?";
+		private final String depositPath = "/v2/reports/deposit-details.csv?";
+    	private Map<String, String> reportRange;
+    	private Map<String, String> urlParams;
+    	private String domainUrl;
     	private String locationName;
     	
-    	public ReportLinks(Location location) {
+    	public ReportLinks(String domainUrl, Location location) {   	
     		// initialize all component values
-    		transactionUrl = "https://squareup.com/v2/reports/transactions.csv?";
-    		itemDetailUrl = "https://squareup.com/v2/reports/items.csv?";
-    		depositUrl = "https://squareup.com/v2/reports/deposit-details.csv?";
+    		this.domainUrl = domainUrl;
     		
-    		// TODO(wtsang): fix this hack to get the correct timestamps for the report URL
-    		beginTime = "begin_time=" + TimeManager.getPastDayInterval(0, 1, "GMT-07:00").get("begin_time");
-    		endTime = "end_time=" + TimeManager.getPastDayInterval(0, 1, "GMT-07:00").get("end_time");
+    		// use TimeManager.java to get correctly formatted begin/end times for the day before (offset 1)
+    		reportRange = TimeManager.getPastDayInterval(0, 1, "GMT-07:00");
     		
-    		// hard-code values for now; update code in the future if necessary
-    		eventBased = "event_based=true"; 
-    		locale = "locale=en";
-    		
-    		// extract relevant info from location object
-    		locationToken = "subunit_merchant_token[]=" + location.getId();
-    		timezone = "Time-Zone=" + location.getTimezone();
-    		locationName = location.getName();    		 		
+    		// add parameters into key/value pairs
+    		urlParams = new HashMap<String, String>();
+    		urlParams.put("begin_time", reportRange.get("begin_time"));
+    		urlParams.put("end_time", reportRange.get("end_time"));
+    	    urlParams.put("event_based", "true");
+    		urlParams.put("locale", "en");
+    		urlParams.put("subunit_merchant_token[]", location.getId());
+    		urlParams.put("Time-Zone", location.getTimezone());
+
+    		locationName = location.getName();		 		
     	}
     	
     	public String getTransactionUrl() {
-    		return transactionUrl + buildParams();
+    		return domainUrl + transactionPath + buildParams();
     	}
     	
     	public String getItemUrl() {   		
-    		return itemDetailUrl + buildParams();
+    		return domainUrl + itemDetailPath + buildParams();
     	}
     	
     	public String getDepositUrl() {   		
-    		return depositUrl + buildParams();
+    		return domainUrl + depositPath + buildParams();
     	}
     	
     	public String getName() {
-    		return locationName;    		
+    		return locationName;
     	}
     	
     	// construct parameters for URL
     	//     begin_time=2016-11-02T00:00:00-07:00&end_time=2016-11-03T00:00:00-07:00&Time-Zone=America/Los_Angeles&event_based=true&locale=en&subunit_merchant_token[]=FXQFKY6YHS7FM
-    	private String buildParams() {
+    	private String buildParams() {    		
     		String params = "";
     		String delim = "&";
     		
-    		params += beginTime + delim + endTime + delim + timezone + delim;
-    		params += eventBased + delim + locale + delim + locationToken;
-    				    		
-    		return params; 		
-    	}	
-    	   	
-    } // end inner class LocationUrls        
+    		for (String key: urlParams.keySet()) {
+    			params += delim;
+    			params += key;
+    			params += "=";
+    			params += urlParams.get(key);
+    		}
+    		params = params.replaceFirst(delim, "");
+    		
+    		return params;
+    	}
+    	
+    } // end inner class LocationUrls
     
 }
