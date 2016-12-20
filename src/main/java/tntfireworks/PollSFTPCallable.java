c06336b119e11efc3dd7731ecb1a23f8ad1be7f1
wtsang <wtsang@squareup.com>
2016-12-19 18:02:25 -0800

    Add dynamic file processing; remove static marketing programs and detect unique programs

--- src/main/java/tntfireworks/PollSFTPCallable.java
+++ src/main/java/tntfireworks/PollSFTPCallable.java
@@ -3,11 +3,9 @@ package tntfireworks;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.TimeZone;
 import java.util.Vector;
@@ -28,13 +26,7 @@ import util.TimeManager;
 public class PollSFTPCallable implements Callable {
     // logger
     private static Logger logger = LoggerFactory.getLogger(PollSFTPCallable.class);
-    
-    // list of file prefixes 
-    //     ex: 2TNTC_<mmddyyyy>.csv or locations_<mmddyyyy>.csv 
-    public static String[] prefixValues = new String[] {"2TNTC", "2TNTSS", "2COK703", "5TNT", 
-                                                       "5AZSAT", "4TCSWA", "4TCSUT", "4TSS", "locations"};
-    public static final HashSet<String> FILE_PREFIXES = new HashSet<String>(Arrays.asList(prefixValues));
-    
+
     // sftp config
     private static String sftpHost;
     private static int sftpPort;
@@ -43,7 +35,7 @@ public class PollSFTPCallable implements Callable {
     private static String sftpBasePath;
     private static String sftpArchivePath;
     private static String sftpInputPath;
-    private static String sftpProcessingPath;   
+    private static String sftpProcessingPath;
 
     public void setSftpHost(String sftpHost) {
         this.sftpHost = sftpHost;
@@ -60,26 +52,26 @@ public class PollSFTPCallable implements Callable {
     public void setSftpPassword(String sftpPassword) {
         this.sftpPassword = sftpPassword;
     }
-    
+
     public void setSftpBasePath(String sftpBasePath) {
         this.sftpBasePath = sftpBasePath;
     }
-    
+
     public void setSftpArchivePath(String sftpArchivePath) {
         this.sftpArchivePath = sftpArchivePath;
     }
-    
+
     public void setSftpInputPath(String sftpInputPath) {
         this.sftpInputPath = sftpInputPath;
     }
-    
+
     public void setSftpProcessingPath(String sftpProcessingPath) {
         this.sftpProcessingPath = sftpProcessingPath;
     }
-    
+
     @Override
     public Object onCall(MuleEventContext eventContext) throws Exception {
-        ChannelSftp sftpChannel = SSHUtil.createConnection(sftpHost, sftpPort, sftpUser, sftpPassword); 
+        ChannelSftp sftpChannel = SSHUtil.createConnection(sftpHost, sftpPort, sftpUser, sftpPassword);
 
         List<SyncToDatabaseRequest> newRequests = getSFTPRequests(sftpChannel);
 
@@ -87,64 +79,109 @@ public class PollSFTPCallable implements Callable {
 
         return newRequests;
     }
-    
-    private List<SyncToDatabaseRequest> getSFTPRequests(ChannelSftp channel) throws InterruptedException, SftpException, ParseException {       
+
+    private List<SyncToDatabaseRequest> getSFTPRequests(ChannelSftp channel)
+            throws InterruptedException, SftpException, ParseException {
         // sftp paths to input/processing directories
         String inputFullPath = sftpBasePath + sftpInputPath;
         String processingFullPath = sftpBasePath + sftpInputPath + sftpProcessingPath;
         String archiveFullPath = sftpBasePath + sftpArchivePath;
-        
-        // targetFiles => total files list containing unique marketing plans and/or locations
-        // sftpRequests => final requests sent for processing
-        HashMap<String, LsEntry> targetFiles = new HashMap<String, LsEntry>();
-        ArrayList<SyncToDatabaseRequest> sftpRequests = new ArrayList<SyncToDatabaseRequest>();               
 
-        // find oldest files per marketing plan
-        for (String filePrefix : FILE_PREFIXES) {
-            Vector<LsEntry> list = channel.ls(String.format("%s/%s_*.csv", inputFullPath, filePrefix));
-            LsEntry targetFile = getOldestFile(list);
-    
-            if (targetFile != null)
-                targetFiles.put(filePrefix, targetFile);                  
+        // filesToProcess => total files list in the input folder marketing plans and/or locations
+        // inputFilesByPrefix => unique prefixes of files located in input folder
+        // sftpRequests => final requests sent for processing
+        HashMap<String, LsEntry> filesToProcess = new HashMap<String, LsEntry>();
+        HashMap<String, ArrayList<LsEntry>> inputFilesByPrefix = new HashMap<String, ArrayList<LsEntry>>();
+        ArrayList<SyncToDatabaseRequest> sftpRequests = new ArrayList<SyncToDatabaseRequest>();
+
+        // get all CSVs located in input folder
+        Vector<LsEntry> inputFiles = channel.ls(String.format("%s/*.csv", inputFullPath));
+
+        // sort all files into lists according to unique prefix
+        for (LsEntry file : inputFiles) {
+            String prefix = "";
+
+            // prepare regex pattern for group matching
+            // ^([A-Za-z0-9]+) => find first occurrence of any combination of letters and numbers
+            // (\\d{8}) => find 8 digits
+            Pattern r = Pattern.compile("^([A-Za-z0-9]+)_(\\d{8}).csv");
+
+            // use regex to find date in currentFile
+            Matcher m = r.matcher(file.getFilename());
+            m.find();
+
+            // parse filename from matched regex
+            try {
+                prefix = m.group(1);
+            } catch (Exception e) {
+                logger.warn(String.format("Exception caught: '%s', could not match file '%s'", e.getCause(),
+                        file.getFilename()));
+
+            }
+
+            if (prefix != "") {
+                ArrayList<LsEntry> prefixedFileList;
+                prefixedFileList = inputFilesByPrefix.get(prefix);
+                if (prefixedFileList == null) {
+                    prefixedFileList = new ArrayList<LsEntry>();
+                    prefixedFileList.add(file);
+                    inputFilesByPrefix.put(prefix, prefixedFileList);
+                }
+
+                prefixedFileList.add(file);
+            }
         }
-        
+
+        // obtain each list of files with the same prefix and find oldest
+        for (String filePrefix : inputFilesByPrefix.keySet()) {
+            LsEntry targetFile = getOldestFile(inputFilesByPrefix.get(filePrefix));
+            if (targetFile != null) {
+                filesToProcess.put(filePrefix, targetFile);
+            }
+        }
+
         // wait for files to finish uploading to SFTP server
         Thread.sleep(7500);
 
         // add to processing folder
-        for (String filePrefix : targetFiles.keySet()) {
+        for (String filePrefix : filesToProcess.keySet()) {
             Boolean processFile = true;
-            LsEntry currentFile = targetFiles.get(filePrefix);
-            String  currentFilename = currentFile.getFilename();
-            
+            LsEntry currentFile = filesToProcess.get(filePrefix);
+            String currentFilename = currentFile.getFilename();
+
             // check if file is still uploading
-            if (currentFile.getAttrs().getSize() !=  channel.lstat(String.format("%s/%s", inputFullPath, currentFilename)).getSize()) {
+            if (currentFile.getAttrs().getSize() != channel
+                    .lstat(String.format("%s/%s", inputFullPath, currentFilename)).getSize()) {
                 processFile = false;
-                logger.info(String.format("%s not ready for processing, still uploading to SFTP server.", currentFilename));
+                logger.info(
+                        String.format("%s not ready for processing, still uploading to SFTP server.", currentFilename));
             }
-            
+
             // check if another file is processing
             if (channel.ls(String.format("%s/*.csv", processingFullPath)).size() > 0) {
-                processFile = false; 
-                logger.info(String.format("Can't begin processing %s. Already processing another file.", currentFilename));
+                processFile = false;
+                logger.info(
+                        String.format("Can't begin processing %s. Already processing another file.", currentFilename));
             }
-            
+
             // move file to processing directory
             if (processFile) {
                 String processingFilename = currentDatestamp() + "_" + currentFilename;
-                channel.rename(String.format("%s/%s", inputFullPath, currentFilename), String.format("%s/%s", processingFullPath, processingFilename));
+                channel.rename(String.format("%s/%s", inputFullPath, currentFilename),
+                        String.format("%s/%s", processingFullPath, processingFilename));
                 logger.info(String.format("Queuing %s for processing (%s)...", currentFilename, processingFilename));
 
                 // create request object to be processed by VM
-                SyncToDatabaseRequest newRequest = new SyncToDatabaseRequest(currentFilename, processingFilename, processingFullPath, archiveFullPath);
+                SyncToDatabaseRequest newRequest = new SyncToDatabaseRequest(currentFilename, processingFilename,
+                        processingFullPath, archiveFullPath);
                 newRequest.setSftpHost(sftpHost);
                 newRequest.setSftpPort(sftpPort);
                 newRequest.setSftpUser(sftpUser);
                 newRequest.setSftpPassword(sftpPassword);
-                
-                sftpRequests.add(newRequest);             
+
+                sftpRequests.add(newRequest);
             }
-        }       
+        }
         return sftpRequests;
     }
 
@@ -162,12 +199,12 @@ public class PollSFTPCallable implements Callable {
     // file formats handled:
     //     1. <marketing plan>_<date mmddyyyy>.csv
     //     2. locations_<date mmddyyyy>.csv
-    private LsEntry getOldestFile(Vector<LsEntry> fileList) throws ParseException {        
+    private LsEntry getOldestFile(ArrayList<LsEntry> fileList) throws ParseException {
         // only proceed if list.size() > 1 
         if (fileList.size() == 0)
             return null;
         else if (fileList.size() == 1)
-            return fileList.get(0);       
+            return fileList.get(0);
 
         // iterate over fileList and return oldest file
         LsEntry oldestFile = null;
@@ -181,11 +218,11 @@ public class PollSFTPCallable implements Callable {
             // use regex to find date in currentFile
             Matcher m = r.matcher(currentFilename);
             m.find();
-            
+
             // parse date from matched regex
             //     ex: 2TNTC_12052016.csv
             SimpleDateFormat sdf = new SimpleDateFormat("MMddyyyy");
-            currentFileDate = sdf.parse(m.group(1));            
+            currentFileDate = sdf.parse(m.group(1));
 
             if (oldestFile == null || oldestFileDate.compareTo(currentFileDate) > 0) {
                 oldestFile = entry;
