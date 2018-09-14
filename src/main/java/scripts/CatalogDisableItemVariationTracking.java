62716c37c59a3de7068232f07fa8f1aaf1648ceb
Ben Hartard <bhartard@squareup.com>
2018-09-13 17:11:20 -0700

    Add script to mass disable inventory tracking

---
+++ src/main/java/scripts/CatalogDisableItemVariationTracking.java
@@ -0,0 +1,52 @@
+package scripts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.squareup.connect.v2.CatalogItemVariation;
+import com.squareup.connect.v2.CatalogObject;
+import com.squareup.connect.v2.ItemVariationLocationOverride;
+import com.squareup.connect.v2.SquareClientV2;
+
+import edu.emory.mathcs.backport.java.util.Arrays;
+import util.SquarePayload;
+
+public class CatalogDisableItemVariationTracking {
+
+    private static String ENCRYPTED_ACCESS_TOKEN = System.getenv("SCRIPT_ENCRYPTED_ACCESS_TOKEN");
+    private static String ENCRYPTION_KEY = System.getenv("SCRIPT_ENCRYPTION_KEY");
+
+    private static String API_URL = "https://connect.squareup.com";
+    private static List<String> varsWhitelist = Arrays.asList(new String[] {});
+
+    public static void main(String[] args) throws Exception {
+        SquarePayload account = new SquarePayload();
+        account.setEncryptedAccessToken(ENCRYPTED_ACCESS_TOKEN);
+
+        SquareClientV2 client = new SquareClientV2(API_URL, account.getAccessToken(ENCRYPTION_KEY));
+
+        CatalogObject[] itemVariations = client.catalog().listItemVariations();
+
+        ArrayList<CatalogObject> variationsToModify = new ArrayList<CatalogObject>();
+        for (CatalogObject catalogObject : itemVariations) {
+            if (varsWhitelist.contains(catalogObject.getId())) {
+                variationsToModify.add(catalogObject);
+
+                CatalogItemVariation itemVariation = catalogObject.getItemVariationData();
+
+                itemVariation.setTrackInventory(false);
+
+                if (itemVariation.getLocationOverrides() != null) {
+                    for (ItemVariationLocationOverride locationOverride : itemVariation.getLocationOverrides()) {
+                        if (locationOverride.isTrackInventory()) {
+                            locationOverride.setTrackInventory(false);
+                        }
+                    }
+                }
+            }
+        }
+
+        CatalogObject[] objectsToModify = variationsToModify.toArray(new CatalogObject[variationsToModify.size()]);
+        client.catalog().batchUpsertObjects(objectsToModify);
+    }
+}
