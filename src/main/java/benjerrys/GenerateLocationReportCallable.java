219f2927b59ab21ba239fee691f1922f36a8c601
Junik Kim <junik@squareup.com>
2023-04-04 11:25:03 -0500

    remove debug email as well

--- src/main/java/benjerrys/GenerateLocationReportCallable.java
+++ src/main/java/benjerrys/GenerateLocationReportCallable.java
@@ -130,7 +130,6 @@ public class GenerateLocationReportCallable implements Callable {
 
     private List<String> setLocationEmailRecipients(MuleMessage message, Location location, Employee[] employees) {
         List<String> emailRecipients = reportRecipients(location, employees);
-    	emailRecipients.add("benjerrys+debug@squareup.com");
         message.setProperty("locationRecipients", String.join(",", emailRecipients), PropertyScope.INVOCATION);
 
         return emailRecipients;
