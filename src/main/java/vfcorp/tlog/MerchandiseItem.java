8748ed3367291025558c024c6766105a66778e61
Junik Kim <junik@squareup.com>
2023-07-25 12:46:36 -0500

    check for null tax values

--- src/main/java/vfcorp/tlog/MerchandiseItem.java
+++ src/main/java/vfcorp/tlog/MerchandiseItem.java
@@ -131,7 +131,7 @@ public class MerchandiseItem extends Record {
         putValue("Extended Total", Integer.toString(lineItem.getGrossSalesMoney().getAmount()));
         putValue("Not On File Indicator", "0"); // not supported
         putValue("Entry Indicator", "1"); // not supported
-        putValue("Taxable Indicator", lineItem.getAppliedTaxes().length == 0 ? "1" : "0");
+        putValue("Taxable Indicator", lineItem.getAppliedTaxes() != null && lineItem.getAppliedTaxes().length == 0 ? "1" : "0");
         putValue("Item Status", "00"); // not supported
         putValue("Raincheck", "0"); // not supported
         putValue("Gift Item", "0"); // not supported
