b5ba75c3ed79532fccf1760f2353ed10e4bd51da
Junik Kim <junik@squareup.com>
2023-08-01 14:00:07 -0500

    add stack trace

--- src/main/java/tntfireworks/reporting/RetrieveMerchantPayloadCallable.java
+++ src/main/java/tntfireworks/reporting/RetrieveMerchantPayloadCallable.java
@@ -236,9 +236,8 @@ public class RetrieveMerchantPayloadCallable implements Callable {
 	                }
 	            }
         	} catch (Exception e) {
-        		logger.error("ERROR: caught exception while aggregating '" + reportType + "' details: " + e.getCause());
+        		logger.error("ERROR: caught exception while aggregating '" + reportType + "' details: " + e.getCause(), e);
         		logger.error(e.getMessage());
-        		e.printStackTrace();
         		logger.error("FAILED LOCATION: " + location.getId() + ", MERCHANT: " + deployment.getMerchantId());
         		logger.error("NUMBER OF LOCATIONS FROM THE FAILED DEPLOYMENT LOCATION: " + locations.length);
         		failedLocationCounter++;
